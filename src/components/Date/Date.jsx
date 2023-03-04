40a49d5408bf29e2967d00b5349ce71a36eccc54
Beka Chaduneli <120742353+bekaChaduneli@users.noreply.github.com>
2023-03-04 13:31:28 +0400

    Add weekday

--- src/components/Date/Date.jsx
+++ src/components/Date/Date.jsx
@@ -1,5 +1,5 @@
 import style from "./Date.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 function DateFunc({
   setClockState,
   clockState,
@@ -8,9 +8,31 @@ function DateFunc({
   setClockStateDay,
   clockStatePm,
 }) {
+  const [time, setTime] = useState(new Date());
+  const [weekdays, setWeekdays] = useState(0);
+  const days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  const currDay = time.getDate();
+  const rightDay = weekdays + currDay;
+  const currentDay = days[rightDay];
   useEffect(() => {
+    if (rightDay > 6) {
+      setWeekdays(-7);
+    }
+    if (rightDay > 13) {
+      setWeekdays(-14);
+    }
+    if (rightDay > 20) {
+      setWeekdays(-21);
+    }
+    if (rightDay > 27) {
+      setWeekdays(-28);
+    }
+    if (rightDay === 1) {
+      setWeekdays(0);
+    }
     setInterval(() => {
       const date = new Date();
+      setTime(date);
       if (date.toLocaleTimeString().length > 10) {
         setClockState(date.toLocaleTimeString().slice(0, 5));
         setClockStatePm(date.toLocaleTimeString().slice(9, 11));
@@ -24,9 +46,14 @@ function DateFunc({
   return (
     <>
       <div>
-        <h1 className={`${style.pm}`}>{clockStatePm}</h1>
-        <h1 className={`${style.time}`}>{clockState}</h1>
-        <h1 className={`${style.day}`}>{clockStateDay}</h1>
+        <div className="flex flex-row gap-2">
+          <h1 className={`${style.pm}`}>{clockStatePm}</h1>
+          <h1 className={`${style.time}`}>{clockState}</h1>
+        </div>
+        <div className="flex flex-row gap-2">
+          <h1 className={`${style.day}`}>{clockStateDay}</h1>
+          <h1 className={`${style.weekday}`}>{currentDay}</h1>
+        </div>
       </div>
     </>
   );
